7cb61825c67e4fd40ece01d6ca5c3ee4c1e0b955
Ricardo Costa <ricardo18acs@gmail.com>
2022-04-30 13:36:23 -0300

    chore: improve events and readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # NodeJs Graceful Shutdown
 
 The best practices to prevent unexpected application crashes, that maintains an open database connection and others unwanted events.
+
+**The Code**
+- [Find main file here](https://github.com/Ricardo-Costa/nodejs-graceful-shutdown/blob/8e9b0bda6d734abebc9307734209164bb9791162/src/index.js)
+
+**Test Function**
+- Start Application
+  - `npm run start`
+  - Or in Debug mode: `npm run start:debug`
+- Press the keys ( Events should be launched )
+  - `ctrl` + `c`
+- Force crash by request ( Events should be launched )
+  - `npm run crash`
--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
-import { createServer } from "http"
+import { createServer } from "http";
+import { once } from "events";
 
-async function handler(request, response) {
+const PORT = 3000;
+
+function requestUtil (req) {
+  return {
+    extractBody: async () => JSON.parse(await once(req, "data"))
+  };
+}
+
+/**
+ * @param {import("http").IncomingMessage} request
+ * @param {import("http").ServerResponse} response
+ */
+async function handler (request, response) {
+  // force crash by any request
+  const { message } = await requestUtil(request).extractBody();
+  if (message) {
+    console.error(message);
+    process.exit(0);
+  }
+
   try {
     response.writeHead(200);
     response.end(JSON.stringify());
   } catch (error) {
-    
+    console.error(error);
+    response.writeHead(500);
+    response.end();
   }
 }
+
+createServer(handler)
+  .listen(PORT)
+  .on("listening", () => console.log(`Application start at port ${PORT}`));
+
+class HandleErrors {
+  static async mainFlow () {
+    console.log("Close DB connection...");
+    console.log("Create some log to AWS CloudWatch OR APM maybe....");
+    console.log("Create a new process for this app if want...");
+    process.exit(0);
+  }
+
+  static async event () {
+    console.log("Do something with this params.");
+    await HandleErrors.mainFlow(...arguments);
+  }
+
+  static async crash () {
+    console.log("Do something with this params.");
+    await HandleErrors.mainFlow(...arguments);
+  }
+}
+
+// catch errors not in try catch statement
+process.on("uncaughtException", HandleErrors.event);
+process.on("uncaughtExceptionMonitor", HandleErrors.event);
+process.on("unhandledRejection", HandleErrors.event);
+
+// unexpected end of application
+process.on("SIGINT", HandleErrors.crash);
+process.on("SIGTERM", HandleErrors.crash);
+process.on("SIGKILL", HandleErrors.crash);
+// process.on("beforeExit", HandleErrors.crash);
+// process.on("exit", HandleErrors.crash);
